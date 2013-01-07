005849fd317582408c69e965d0924a4262d64db3
Mahbub Rony <rony.cse36@gmail.com>
2013-01-08 00:23:30 +0600

    new apps lunched

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNewRailsApps::Application.config.secret_token = 'aa3e78f01eaff660b9e47e6b46c5760772015bf48c944fb26aabe7eedc1c967ce08d6c05d6d2c6d1c2e5dac00ea46adbb36f8e78de5f4c5d2ac85dfeca9dfbca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyNewRailsApps::Application.config.session_store :cookie_store, key: '_my_new_rails_apps_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyNewRailsApps::Application.config.session_store :active_record_store
